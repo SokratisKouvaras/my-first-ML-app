d01b8aa2565dfb18ad3ffe97197904976abcfbf8
Sokratis <sokrateskouv@gmail.com>
2022-07-10 15:57:20 +0200

    adapt uploading\

--- app/app.py
+++ app/app.py
@@ -1,12 +1,14 @@
-from flask import Flask, Response, render_template, request, make_response
+from flask import Flask, Response, render_template, request, make_response, redirect, url_for
 from static import *
 import os
 import pickle
 import sklearn
+import csv
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 app.debug = False
 
+uploaded_file = []
 model1 = pickle.load(open('ml_models/model.pickle', 'rb'))
 
 
@@ -25,8 +27,6 @@ def model1(name=None):
 
 @app.route("/model1/download", methods=["GET"])
 def getPredictionsCSV():
-    # with open("outputs/Adjacency.csv") as fp:
-    #     csv = fp.read()
     csv = "1,2,3\n4,5,6\n"
     return Response(
         csv,
@@ -38,13 +38,18 @@ def getPredictionsCSV():
 @app.route("/model1/upload", methods=["GET", "POST"])
 def uploadmodel1():
     try:
-        uploaded_file = request.files["file"]
-        if uploaded_file.filename != "":
-            filepath = os.path.join("static/files", uploaded_file.filename)
-            uploaded_file.save(filepath)
-            print("FILE SAVED")
-            return Response(status=200)
-    except:
+        user_file = request.files["file"]
+        if user_file.filename != "":
+            filepath = os.path.join("static/files", user_file.filename) 
+            user_file.save(filepath)
+            with open(filepath, encoding="utf8") as file:
+                csv_file = csv.reader(file,delimiter=';')
+                for row in csv_file:
+                    uploaded_file.append(row)
+            return redirect(url_for("model1"))
+
+    except Exception as e:
+        print(f"Exception:{e}")
         resp = make_response(render_template("error.html"), 404)
         return resp
 
